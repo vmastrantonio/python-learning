81bf69d8ce4ff973c3bcbb1812ed35877054526d
Vitor Fernandes <vitorfeernandes@icloud.com>
2023-10-13 14:14:47 -0300

    #1

---
+++ Exercicios/1 - Conhecendo a linguagem (1~2)/exercicio_001.py
@@ -0,0 +1,4 @@
+﻿# Crie um programa que escreva "Olá, mundo" na tela.
+
+msg = "Olá, mundo!"
+print(msg)
---
+++ Exercicios/1 - Conhecendo a linguagem (1~2)/exercicio_002.py
@@ -0,0 +1,10 @@
+﻿# Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas.
+
+entrada = input("Digite seu nome:")
+print(f"Boas vindas, {entrada}!")
+
+dia = int(input("Dia de nascimento:"))
+mes = int(input("Mês de nascimento:"))
+ano = int(input("Ano de nascimento:"))
+
+print(f"A pessoa nasceu no dia {dia} do mês {mes} do ano de {ano}.")
