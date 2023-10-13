7bd445d0955ea06505f19e4ff895e0c20c2a5619
Vitor Fernandes <vitorfeernandes@icloud.com>
2023-10-13 14:16:18 -0300

    #2

---
+++ Exercicios/2 - Primeiros passos (3~4)/exercicio_003.py
@@ -0,0 +1,6 @@
+﻿# Crie um programa que leia dois números e mostre a soma entre eles.
+
+num1 = int(input("Primeiro número: "))
+num2 = int(input("Segundo número: "))
+
+print(f"A soma é: {num1 + num2}")
---
+++ Exercicios/2 - Primeiros passos (3~4)/exercicio_004.py
@@ -0,0 +1,12 @@
+﻿# Faça um programa que leia algo pelo teclado e mostre na tela o seu tipo primitivo e todas as informações possiveis sobre ele.
+
+entrada = input("Digite algo: ")
+
+print(f"Tipo primitivo: {type(entrada)}")
+
+print(f"É numérico? {entrada.isnumeric()}")
+print(f"É alfanumérico? {entrada.isalpha()}")
+print(f"É um decimal? {entrada.isdecimal()}")
+print(f"Está em caixa baixa? {entrada.islower()}")
+print(f"É apenas espaço em branco? {entrada.isspace()}")
+print(f"Está em caixa alta? {entrada.isupper()}")
